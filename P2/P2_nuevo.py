37089479647762ab088aa2e12e9c606ed6b4431c
Jesús Doménech <jesusjda@gmail.com>
2015-03-06 16:07:34 +0000

    P2 casi funcionando

--- P2/P2_nuevo.py
+++ P2/P2_nuevo.py
@@ -7,42 +7,24 @@ from __future__ import division
 
 import numpy as np #para crear arrays de puntos
 from scipy.integrate import odeint #resolutor de edo
-
+import matplotlib.pyplot as pp
 import math #funciones matematicas
-
 import sympy as sp  #calculo simbolico
 from sympy.utilities.lambdify import lambdify
 
 import matplotlib.pyplot as plt
 
 from mayavi import mlab
-
+ddU = lambda (u,v,du,dv): 0
 def rhs_eqs(Y, _):
+    global ddU
     u,v,du,dv = Y
     return [du,dv,ddU(u,v,du,dv)[0][0], ddU(u,v,du,dv)[0][1]]
 
-
-if __name__ == '__main__':
-    u,v,du,dv = sp.symbols('u v du dv')
-
-    ## Caso (b). Entrada I, u0, v0, du0, dv0
-    r, a = 2.0, 5.0 #radios del toro
-    
-    E = r**2
-    F = 0.0
-    G = (r*sp.cos(u)+a)**2
-
-    #E=1.0/v**2
-    #G=1.0/v**2
-    #F=0
+def plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits):
+    global ddU
     I = sp.Matrix([[E, F], [F, G]])
-    
-    u0, v0 = np.pi, 0.0
-    du0, dv0 = 0.0, 1.0
-    init_cond = [u0, v0, du0, dv0]
-
-    t = [0, 50*np.pi]
-    delta = 0.05
+    delta = 0.005
     interval = np.arange(t[0], t[1]+delta, delta)
     #fin de la entrada
 
@@ -56,60 +38,108 @@ if __name__ == '__main__':
     A = 0.5 * sp.Matrix([A1 ,A2]).transpose()
     B = dU.transpose() * (dIu*du+dIv*dv)
 
- 
     ddU = lambdify((u, v, du, dv), (A - B)*I.inv(), [{'ImmutableMatrix': np.array}, 'numpy'])
     
     solu = odeint(rhs_eqs, init_cond, interval)
-    x_coord2 = [x for [x,y,dx,dy] in solu]
-    y_coord2 = [y for [x,y,dx,dy] in solu]
-    plt.plot(x_coord2,y_coord2);
-    plt.axis('equal')
+    x_coord2 = [(x%(u_limits[1]-u_limits[0])+u_limits[0]) for [x,y,dx,dy] in solu]
+    y_coord2 = [(y%(v_limits[1]-v_limits[0])+v_limits[0]) for [x,y,dx,dy] in solu]
+    fig = plt.plot(x_coord2,y_coord2,',')
     plt.show()
 
+    return solu
 
 
 
+if __name__ == '__main__':
+    u,v,du,dv = sp.symbols('u v du dv') 
 
+    ##########################################
+    #  Caso (a). Entrada x(u,v), intervalo_u, intervalo_v
+    ##########################################
+    caso = 0
 
+    u_limits = [0,2.0*np.pi]
+    v_limits = [0,2.0*np.pi]
+    t_limits = [0, 16*np.pi]
+
+    u0, v0 = np.pi, 0.0
+    du0, dv0 = 1.0, 1.0
+    init_cond = [u0, v0, du0, dv0]
+
+    E,F,G = calculoff()
+    
+    # Fin entrada caso (a)
+
+    ########################################## 
+    #  Caso (b). Entrada I, u0, v0, du0, dv0 #
+    ##########################################
+    caso = 1
+
+    r, a = 2.0, 5.0 #radios del toro
+    E = r**2
+    F = 0.0
+    G = (r*sp.cos(u)+a)**2
+
+    u_limits = [0,2.0*np.pi]
+    v_limits = [0,2.0*np.pi]
+    t_limits = [0, 16*np.pi]
+
+    u0, v0 = np.pi, 0.0
+    du0, dv0 = 1.0, 1.0
+    init_cond = [u0, v0, du0, dv0]
+
+    #E=1.0/v**2
+    #G=1.0/v**2
+    #F=0
+    
+    # Fin entrada caso (b)
     #---------------------------------------------------------------------------
-    X = lambdify((u,v), ((r*sp.cos(u)+a)*sp.cos(v), (r*sp.cos(u)+a)*sp.sin(v), r*sp.sin(u)),[{'ImmutableMatrix': np.array}, 'numpy'])
 
-    x_coord =[]
-    y_coord =[]
-    z_coord =[]
+    solu = plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits)
 
-    for s in solu:
-        aux = X(s[0],s[1])
-        x_coord.append(aux[0])
-        y_coord.append(aux[1])
-        z_coord.append(aux[2])
+    if caso == 0:
+        plot_surface(X,solu)
+
+
+ 
+    # X = lambdify((u,v), ((r*sp.cos(u)+a)*sp.cos(v), (r*sp.cos(u)+a)*sp.sin(v), r*sp.sin(u)),[{'ImmutableMatrix': np.array}, 'numpy'])
+
+    # x_coord =[]
+    # y_coord =[]
+    # z_coord =[]
+
+    # for s in solu:
+    #     aux = X(s[0],s[1])
+    #     x_coord.append(aux[0])
+    #     y_coord.append(aux[1])
+    #     z_coord.append(aux[2])
     
 
-    mlab.plot3d(x_coord, y_coord, z_coord)    
+    # mlab.plot3d(x_coord, y_coord, z_coord)    
 
 
 
 
 
     
-    cu = np.linspace(0, 2*np.pi, 100)
-    cv = np.linspace(0, 2*np.pi, 100)
+    # cu = np.linspace(0, 2*np.pi, 100)
+    # cv = np.linspace(0, 2*np.pi, 100)
 
-    # Creamos la rejilla con la cual vamos a hacer las cuentas
-    # meshgrid crea el reticulo para hacer operaciones con todo
-    # el reticulo a al vez
-    u_mesh, v_mesh = np.meshgrid(cu, cv)
+    # # Creamos la rejilla con la cual vamos a hacer las cuentas
+    # # meshgrid crea el reticulo para hacer operaciones con todo
+    # # el reticulo a al vez
+    # u_mesh, v_mesh = np.meshgrid(cu, cv)
     
     
-    # Introducimos las formulas de nuetra superficie parametrizada
-    # En este caso, un toro parametrizado
-    cx = (r*np.cos(u_mesh)+a)*np.cos(v_mesh)
-    cy = (r*np.cos(u_mesh)+a)*np.sin(v_mesh)
-    cz = r*np.sin(u_mesh)
+    # # Introducimos las formulas de nuetra superficie parametrizada
+    # # En este caso, un toro parametrizado
+    # cx = (r*np.cos(u_mesh)+a)*np.cos(v_mesh)
+    # cy = (r*np.cos(u_mesh)+a)*np.sin(v_mesh)
+    # cz = r*np.sin(u_mesh)
     
-    #mlab.plot3d(x_curve, y_curve, z_curve)
-    mlab.mesh(cx,cy,cz, colormap='spring')
+    # #mlab.plot3d(x_curve, y_curve, z_curve)
+    # mlab.mesh(cx,cy,cz, colormap='spring')
     
     
     
-    mlab.show()
+    # mlab.show()
