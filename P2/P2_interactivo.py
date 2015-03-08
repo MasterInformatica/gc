526b956d8bf2ecebacba74c5ea7a5b0a675c5797
Jesús Doménech <jesusjda@gmail.com>
2015-03-08 18:13:38 +0000

    P2_interactivo definitivo

--- P2/P2_interactivo.py
+++ P2/P2_interactivo.py
@@ -22,7 +22,6 @@ from mayavi import mlab
 class GenInitConds:
     def __init__ (self):
         self.init_cond = None
-        self.manual = True # Poner false para no sacar la ventana
         self.u0 = None
         self.v0 = None
         self.dv0 = None
@@ -49,8 +48,8 @@ class GenInitConds:
         self.t_limits = t_limits
         self.fig = plt.figure()
         self.ax = plt.subplot(111)
-        self.ax.set_xlim(u_limits[0],u_limits[1])
-        self.ax.set_ylim(v_limits[0],v_limits[1])
+        self.ax.set_xlim(max(u_limits[0],-30),min(u_limits[1],30))
+        self.ax.set_ylim(max(v_limits[0],-30),min(v_limits[1],30))
         self.ax.set_title('Pincha dos puntos para dibujar la geodesica con ese vector.')
         self.evento_click = self.fig.canvas.mpl_connect(
             'button_press_event', self.on_clk)
@@ -84,8 +83,6 @@ def rhs_eqs(Y, _):
 # calcula y dibuja una geodesica con init_cond
 def plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits): 
     global ddU
-    delta = 0.005
-    interval = np.arange(t_limits[0], t_limits[1]+delta, delta)
 
     I = sp.Matrix([[E, F], [F, G]])
     dIu = I.diff(u)
@@ -100,9 +97,11 @@ def plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits):
 
     ddU = lambdify((u, v, du, dv), (A - B)*I.inv(), [{'ImmutableMatrix': np.array}, 'numpy'])
     
-    #solu = odeint(rhs_eqs, init_cond, interval)
-
+    ####################################
+    #      Punto inicial y delta       #
+    ####################################
     t0 = 0
+    delta = 0.005
 
     if t0 == t_limits[0]: #el punto es el limite inferior
         interval = np.arange(t0, t_limits[1]+delta, delta)
@@ -113,21 +112,20 @@ def plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits):
     else: #se encuentra en el interior
         interval1 = np.arange(t0, t_limits[0]-delta, -delta)
         interval2 = np.arange(t0, t_limits[1]+delta, delta)
-
         solu1 = odeint(rhs_eqs, init_cond, interval1)
         solu2 = odeint(rhs_eqs, init_cond, interval2)
 
         solu = np.concatenate((solu1, solu2))
 
-
+    # Se meten los valores dentro del intervalo de u y v para dibujarlo    
     x_coord2 = [(x%(u_limits[1]-u_limits[0])+u_limits[0]) for [x,y,dx,dy] in solu]
     y_coord2 = [(y%(v_limits[1]-v_limits[0])+v_limits[0]) for [x,y,dx,dy] in solu]
 
-    x_coord2 = [x for [x,y,dx,dy] in solu]
-    y_coord2 = [y for [x,y,dx,dy] in solu]
+    #x_coord2 = [x for [x,y,dx,dy] in solu]
+    #y_coord2 = [y for [x,y,dx,dy] in solu]
     
     fig = plt.plot(x_coord2,y_coord2,',')
-    plt.axis([-20.0, 20.0, 0.0, 20.0])
+    plt.axis([max(u_limits[0],-30),min(u_limits[1],30),max(v_limits[0],-30),min(v_limits[1],30)])
     plt.show()
 
     return solu
@@ -172,22 +170,25 @@ if __name__ == '__main__':
     u,v,du,dv = sp.symbols('u v du dv') 
     # Si quieres introducir las condiciones iniciales graficamente
     # pon esta variable a True, para dar [u0,v0,du0,dv0] desde plot
+   
     init_cond_plot = True
+
     ##########################################
     #  Caso (a). Entrada x(u,v), intervalo_u, intervalo_v
     ##########################################
     caso = 0
     r, a = 2.0, 5.0
-
+    
     x = (r*sp.cos(u)+a)*sp.cos(v)
     y = (r*sp.cos(u)+a)*sp.sin(v)
     z = r*sp.sin(u)
-
-    # X(u,v) = (x(u,v),y(u,v),z(u,v))
-    X = sp.Matrix([x,y,z])
-
-    E,F,G = calculaff(X)
     
+    ## X(u,v) = (x(u,v),y(u,v),z(u,v))
+    X = sp.Matrix([x,y,z])
+    E,F,G = calculaff(X)
+
+    ## Modificar abajo los limites de u,v y t
+
     # Fin entrada caso (a)
 
     ########################################## 
@@ -195,24 +196,37 @@ if __name__ == '__main__':
     ##########################################
     #caso = 1
 
+    # #TORO
     # r, a = 2.0, 5.0 #radios del toro
     # E = r**2
     # F = 0.0
     # G = (r*sp.cos(u)+a)**2
 
-    E=1.0/v**2
-    G=1.0/v**2
-    F=0
+    # #PoinCare
+    #E=1.0/v**2
+    #G=1.0/v**2
+    #F=0
+
+    ## Modificar abajo los limites de u,v y t
     
     # Fin entrada caso (b)
-    #---------------------------------------------------------------------------
+    
+    ########################################## 
+    #           Limites de u, v y t          #
+    ########################################## 
 
-    u_limits = [-99999.99999,99999.9999]
-    v_limits = [0.00001, 99999.99999]
+    u_limits = [0,2*np.pi] #[-9999.9999,9999.9999]
+    v_limits = [0,2*np.pi] #[0.0001, 9999.9999]
     t_limits = [-100.0, 100.0]
 
+    #---------------------------------------------------------------------------
+
+    ########################################## 
+    #         Condiciones iniciales          #
+    ########################################## 
+
     if not init_cond_plot:
-        u0, v0 = np.pi, 0.0
+        u0, v0 = np.pi, 0.1
         du0, dv0 = 1.0, 1.0
         init_cond = [u0, v0, du0, dv0]
         solu = plot_geodesic(E,F,G,init_cond,u_limits,v_limits,t_limits)
@@ -224,52 +238,6 @@ if __name__ == '__main__':
         solu = gen.solu
     print init_cond
 
-
-    
-
     if caso == 0:
         plot_surface(X,solu,u_limits,v_limits)
 
-
-    
-
-
-    # x_coord =[]
-    # y_coord =[]
-    # z_coord =[]
-
-    # for s in solu:
-    #     aux = X(s[0],s[1])
-    #     x_coord.append(aux[0])
-    #     y_coord.append(aux[1])
-    #     z_coord.append(aux[2])
-    
-
-    # mlab.plot3d(x_coord, y_coord, z_coord)    
-
-
-
-
-
-    
-    # cu = np.linspace(0, 2*np.pi, 100)
-    # cv = np.linspace(0, 2*np.pi, 100)
-
-    # # Creamos la rejilla con la cual vamos a hacer las cuentas
-    # # meshgrid crea el reticulo para hacer operaciones con todo
-    # # el reticulo a al vez
-    # u_mesh, v_mesh = np.meshgrid(cu, cv)
-    
-    
-    # # Introducimos las formulas de nuetra superficie parametrizada
-    # # En este caso, un toro parametrizado
-    # cx = (r*np.cos(u_mesh)+a)*np.cos(v_mesh)
-    # cy = (r*np.cos(u_mesh)+a)*np.sin(v_mesh)
-    # cz = r*np.sin(u_mesh)
-    
-    # #mlab.plot3d(x_curve, y_curve, z_curve)
-    # mlab.mesh(cx,cy,cz, colormap='spring')
-    
-    
-    
-    # mlab.show()
