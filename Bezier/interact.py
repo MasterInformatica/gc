651c331cd99d5767a7bdebd7ec1cb6b3f7f7e452
Jesús Doménech <jesusjda@gmail.com>
2015-03-12 12:23:12 +0000

    bezier borra la anterior

--- Bezier/interact.py
+++ Bezier/interact.py
@@ -20,8 +20,8 @@ class DrawPoints:
         self.poly = None    
         self.last_curve = None
         self.cid_press = fig.canvas.mpl_connect('button_press_event', self.on_press)
-#        self.cid_move = fig.canvas.mpl_connect('motion_notify_event', self.on_move)
-#        self.cid_release_button = fig.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cid_move = fig.canvas.mpl_connect('motion_notify_event', self.on_move)
+        self.cid_release_button = fig.canvas.mpl_connect('button_release_event', self.on_release)
 
 
     def on_press(self, event):
@@ -45,31 +45,29 @@ class DrawPoints:
             self.last_curve = self.ax.add_line(self.curve.plot_bezier())   
         self.fig.canvas.draw()  
         
-        
-        
-#        for circle in self.ax.patches:
-#            contains, attr = circle.contains(event)
-#            if contains:
-#                self.touched_circle = circle
-#                self.exists_touched_circle = True
-#                self.pressed_event = event
-#                self.touched_x0, self.touched_y0 = circle.center
-#                return
+        for circle in self.ax.patches:
+            contains, attr = circle.contains(event)
+            if contains:
+                self.touched_circle = circle
+                self.exists_touched_circle = True
+                self.pressed_event = event
+                self.touched_x0, self.touched_y0 = circle.center
+                return
          
         
-#    def on_move(self, event):
-#        if self.exists_touched_circle:
-#            dx = event.xdata - self.pressed_event.xdata
-#            dy = event.ydata - self.pressed_event.ydata
-#            x0, y0 = self.touched_circle.center
-#            self.touched_circle.center = self.touched_x0+dx, self.touched_y0+dy
-#            # Calculos
-#            # Deberia actualizar la curva
-#            self.fig.canvas.draw()
-#            
-#    def on_release(self, event):
-#        self.exists_touched_circle = False
-#        return
+    def on_move(self, event):
+        if self.exists_touched_circle:
+            dx = event.xdata - self.pressed_event.xdata
+            dy = event.ydata - self.pressed_event.ydata
+            x0, y0 = self.touched_circle.center
+            self.touched_circle.center = self.touched_x0+dx, self.touched_y0+dy
+            # Calculos
+            # Deberia actualizar la curva
+            self.fig.canvas.draw()
+            
+    def on_release(self, event):
+        self.exists_touched_circle = False
+        return
 
 if __name__ == '__main__':
     fig = plt.figure()
