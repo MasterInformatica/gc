6933bbd7299d987ec224ae1e9db6a5133f55b9c4
Jesús Doménech <jesusjda@gmail.com>
2015-03-12 12:57:30 +0000

    move bezier

--- Bezier/interact.py
+++ Bezier/interact.py
@@ -13,29 +13,36 @@ class DrawPoints:
         self.fig = fig
         self.ax = ax
         self.exists_touched_circle = False
-        # Le asociamos una etiqueta como que hemos presionado el boton
-        # que despues podemos usar para matar el proceso (aunque tambien
-        # se puede cerrando en la x xD)
+
         self.points = []
         self.poly = None    
         self.last_curve = None
+        self.touched_index = None
+        self.touched_x0, self.touched_y0 = None,None
         self.cid_press = fig.canvas.mpl_connect('button_press_event', self.on_press)
         self.cid_move = fig.canvas.mpl_connect('motion_notify_event', self.on_move)
         self.cid_release_button = fig.canvas.mpl_connect('button_release_event', self.on_release)
 
 
     def on_press(self, event):
-        if event.xdata == None or event.ydata == None:
-            #press out of plot
+        if event.xdata == None or event.ydata == None: #press out of plot
             return
-        
+        for circle in self.ax.patches:
+            contains, attr = circle.contains(event)
+            if contains:
+                self.touched_circle = circle
+                self.exists_touched_circle = True
+                self.pressed_event = event
+                self.touched_x0, self.touched_y0 = circle.center
+                punto = [self.touched_x0, self.touched_y0]
+                self.touched_index = self.points.index(punto)
+                return
         self.points.append([event.xdata, event.ydata])       
         self.poly = np.array(self.points)
         print self.poly
         c = Circle((event.xdata, event.ydata), 0.5)
         self.ax.add_patch(c)
         self.fig.canvas.draw() 
-
          
         self.curve=cb(self.poly)   
         self.curve.plot_bezier()
@@ -45,23 +52,23 @@ class DrawPoints:
             self.last_curve = self.ax.add_line(self.curve.plot_bezier())   
         self.fig.canvas.draw()  
         
-        for circle in self.ax.patches:
-            contains, attr = circle.contains(event)
-            if contains:
-                self.touched_circle = circle
-                self.exists_touched_circle = True
-                self.pressed_event = event
-                self.touched_x0, self.touched_y0 = circle.center
-                return
+
          
         
     def on_move(self, event):
+        if event.xdata == None or event.ydata == None: #press out of plot
+            return
         if self.exists_touched_circle:
             dx = event.xdata - self.pressed_event.xdata
             dy = event.ydata - self.pressed_event.ydata
             x0, y0 = self.touched_circle.center
             self.touched_circle.center = self.touched_x0+dx, self.touched_y0+dy
             # Calculos
+            self.points[self.touched_index] = [self.touched_x0+dx, self.touched_y0+dy]
+            self.poly = np.array(self.points)
+            print self.poly
+            self.curve=cb(self.poly) 
+            self.last_curve.set_data(self.curve.update_bezier())
             # Deberia actualizar la curva
             self.fig.canvas.draw()
             
