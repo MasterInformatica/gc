b0463b4547d29e9561264ffdaf842e985aa2e3cf
Jenny Hernández <jhdez.becares@gmail.com>
2015-03-19 00:15:11 +0000

    Modificado un errorcillo y añadidos muchos comentarios para que se entienda algo y quede súper mega hiper bonito

--- P3/Bezier/casteljau.py
+++ P3/Bezier/casteljau.py
@@ -9,54 +9,72 @@ from matplotlib.lines import Line2D
 class CurvaDeBezier:
     def __init__(self, polygon):
         self.polygon = polygon
+        
+        # Inicializamos N como el numero de puntos - 1, ya que lo usaremos en los
+        # bucles, que iran de 0 a N, con N incluida
         self.N = polygon.shape[0] - 1
         
+        # Inicializaciones varias
         self.num_points = 100
         self.t = np.linspace(0, 1, self.num_points)
-        # quitar esto
-        #self.t2 = np.array(self.t)
-
         self.curve_x = None
         self.curve_y = None
         self.curve = None
-        self._bernstein = np.zeros((self.N + 1, self.num_points))
-        # casteljau = [k,t,i,R2]
-        self._casteljau = np.zeros((self.num_points,self.num_points,self.num_points,2)) #FIXME
-        self._compute_bernstein() # 
-        self._compute_casteljau()
-        self.compute_curve() # for Bernstein
+
+        # Variable que usamos para saber si usar bernstein o casteljau
+        # Si esta variable es True, se usa bernstein. En caso contrario, Casteljau
+        self.compute_bernstein = True ########## CAMBIAR VALOR a False para computar por Casteljau ##########
+
+        # Inicializamos _bernstein y _casteljau como arrays de numpy. 
+        # Estas variables seran las que guarden los valores computados
+        #   por cada uno de los algoritmos respectivamente
+        if self.compute_bernstein == True:
+            self._bernstein = np.zeros((self.N + 1, self.num_points))
+            self._compute_bernstein() 
+        else: 
+            self._casteljau = np.zeros((self.num_points,self.num_points,self.num_points,2)) 
+            self._compute_casteljau()
         
-        #self.plot_bezier()
+        # Calculamos la curva de Bezier
+        self.compute_curve() 
         
+    # Calculos de los polinomios de Bernstein
     def _compute_bernstein(self):
         for i in range(self.N + 1):
             self._bernstein[i, :] = binom(self.N, i) * self.t**i *(1-self.t)**(self.N-i)
 
+    # Calculos mediante el algoritmo de Casteljau
     def _compute_casteljau(self):
+        # Inicializamos la primera columna con los puntos del polinomio introducido
         self._casteljau[0,:,:self.N+1,:] = self.polygon
+        
+        # Vamos rellenando el array por columnas, ya que cada b_i^k depende de la
+        # columna anterior
         for k in range(0, self.N):
             for i in range(0,self.N-k+1):
-                self._casteljau[k+1,:,i, 0]  = (1-self.t)*self._casteljau[k,:,i, 0] + self.t*self._casteljau[k,:,i+1,0]
-                self._casteljau[k+1,:,i, 1]  = (1-self.t)*self._casteljau[k,:,i, 1] + self.t*self._casteljau[k,:,i+1,1]
+                self._casteljau[k+1,:,i, 0] = (1-self.t)*self._casteljau[k,:,i, 0] + self.t*self._casteljau[k,:,i+1,0]
+                self._casteljau[k+1,:,i, 1] = (1-self.t)*self._casteljau[k,:,i, 1] + self.t*self._casteljau[k,:,i+1,1]
         
     
+    # Realizamos los calculos de (x,y) de la curva. 
+    # Si compute_bernstein es False, asignamos a nuestra curva las soluciones dadas
+    # por Casteljau.
     def compute_curve(self):
-        if False:
+        if self.compute_bernstein == True:
             self.curve_x = sum(self.polygon[i, 0] * self._bernstein[i, :] for i in range(self.N + 1))
             self.curve_y = sum(self.polygon[i, 1] * self._bernstein[i, :] for i in range(self.N + 1))
         else:
             self.curve_x = self._casteljau[self.N,:,0, 0]
             self.curve_y = self._casteljau[self.N,:,0, 1]
         
+    
     def plot_bezier(self):
         self.curve = Line2D(self.curve_x, self.curve_y)
-         
         return self.curve        
 
+    # Computamos la nueva curva y devolvemos (x,y) actualizadas
     def update_bezier(self):
         self.compute_curve()
         return (self.curve_x,self.curve_y)
-        #self.ax.add_line(self.curve)
-        #self.fig.canvas.draw()
         
         
--- P3/Bezier/interact.py
+++ P3/Bezier/interact.py
@@ -4,17 +4,14 @@ from matplotlib.patches import Polygon
 from matplotlib.lines import Line2D
 import numpy as np
 
-#from bezier import CurvaDeBezier as cb
 from casteljau import CurvaDeBezier as cb
 
-# from geodesicas import Geodesica
 
 class DrawPoints:
     def __init__(self, fig, ax): #, geodesica):
         self.fig = fig
         self.ax = ax
         self.exists_touched_circle = False
-
         self.points = []
         self.poly = None    
         self.last_curve = None
@@ -30,6 +27,8 @@ class DrawPoints:
             return
         for circle in self.ax.patches:
             contains, attr = circle.contains(event)
+            # Si estamos pinchando en un punto que ya existia, ponemos la variable
+            # exists_touched_circle a True. Esto nos servira en la funcion on_move.
             if contains:
                 self.touched_circle = circle
                 self.exists_touched_circle = True
@@ -38,15 +37,17 @@ class DrawPoints:
                 punto = [self.touched_x0, self.touched_y0]
                 self.touched_index = self.points.index(punto)
                 return
+        # Anyadimos el nuevo punto que hemos pintado y lo dibujamos
         self.points.append([event.xdata, event.ydata])       
         self.poly = np.array(self.points)
-        print 'self.poly', self.poly
         c = Circle((event.xdata, event.ydata), 0.5)
         self.ax.add_patch(c)
         self.fig.canvas.draw() 
          
+        # Calculamos nuestra curva en funcion del poligono que hemos pintado
         self.curve=cb(self.poly)   
-        print 'shape', self.poly.shape[0]
+        # Comprobamos que no haya un solo punto. En caso de haberlo, solo se
+        # pinta ese punto.
         if self.poly.shape[0] > 1:
             self.curve.plot_bezier()
             if self.last_curve != None:
@@ -60,30 +61,34 @@ class DrawPoints:
     def on_move(self, event):
         if event.xdata == None or event.ydata == None: #press out of plot
             return
+        # Si habiamos pinchado encima de un circulo existente y lo intentamos
+        # mover, se actualiza el poligono en consecuencia, se modifican los
+        # valores, se calcula la nueva curva de Bezier y se pinta de nuevo
         if self.exists_touched_circle:
             dx = event.xdata - self.pressed_event.xdata
             dy = event.ydata - self.pressed_event.ydata
             x0, y0 = self.touched_circle.center
             self.touched_circle.center = self.touched_x0+dx, self.touched_y0+dy
+           
             # Calculos
             self.points[self.touched_index] = [self.touched_x0+dx, self.touched_y0+dy]
             self.poly = np.array(self.points)
             print self.poly
-            self.curve=cb(self.poly) 
-            self.last_curve.set_data(self.curve.update_bezier())
-            # Deberia actualizar la curva
+            self.curve=cb(self.poly) # Recalculamos la curva
+            self.last_curve.set_data(self.curve.update_bezier()) # Actualizamos los valores
+
+            # Actualizamos el dibujo con la nueva curva
             self.fig.canvas.draw()
             
     def on_release(self, event):
+        # Reseteamos la variable al soltar el boton del raton
         self.exists_touched_circle = False
         return
 
 if __name__ == '__main__':
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect=1) # aspect=1 no cambia las proporciones
-    ax.set_xlim(-20,20) #Limites de los ejes
+    ax.set_xlim(-20,20) # Limites de los ejes
     ax.set_ylim(-20,20)
-    # geodesica = Geodesica()
     draw_points = DrawPoints(fig, ax)
     plt.show()
-#
