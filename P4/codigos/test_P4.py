674903c822607068625c76de34fe53030a6c9f8f
Jenny Hernández <jhdez.becares@gmail.com>
2015-03-25 09:45:34 +0000

    Código del test

---
+++ P4/codigos/test_P4.py
@@ -0,0 +1,35 @@
+import unittest
+import timeit
+import numpy as np
+from intersect_bezier import IntersectBezier
+
+
+ 
+def eval_intersect(N, epsilon):
+    P = np.random.uniform(-20, 20, (N + 1, 2))
+    Q = np.random.uniform(-20, 20, (N + 1, 2))
+    return intersect(P, Q, epsilon)
+
+
+    
+if __name__ == '__main__':
+    
+    N = 15  
+    epsilon = 0.1
+    intersect = IntersectBezier()
+    
+    intersect(P, Q, epsilon)
+
+    #unittest.main()
+    print(timeit.repeat("eval_intersect(N, epsilon)",
+                        setup="from __main__ import eval_intersect, intersect, N, epsilon",
+                        number=10))
+
+
+
+#tiempos obtenidos en el aula III
+#[2.9526281356811523, 3.1017611026763916, 3.3952529430389404]
+    
+   
+    
+    
