41929be84ff166eae9d5247a508cb8e8cf388802
Jenny Hernández <jhdez.becares@gmail.com>
2015-03-25 09:56:33 +0000

    Añadida la estructura inicial del código de la p4

---
+++ P4/codigos/intersection_bezier.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import binom
+from matplotlib.patches import Polygon
+from matplotlib.lines import Line2D
+
+class IntersectionBezier:
+    def __init(self):
+        # Aqui habra cosas bonitas para la creacion del Intersection
+
+    def __call__(self, P, Q, epsilon):
+        # P.shape = (n, 2)
+        # Q.shape = (m,2)
+        # Epsilon controlara si se aproxima el poligono 
+        #   de control mediante un segmento
+
+        # Returns: K puntos de interseccion calculados como un array
+        # de numpy de dimensiones (K, 2)
+
+        
+    def plot(self, k=3):
+        # Metodo que produce el dibujo de las curvas y las
+        # intersecciones.
+    
