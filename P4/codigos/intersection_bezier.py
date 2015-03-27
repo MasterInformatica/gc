490280a408acfaa86a6aac9db9aaca960e2853a3
Jesús Doménech <jesusjda@gmail.com>
2015-03-27 13:59:18 +0000

    not working but plot!

--- P4/codigos/intersection_bezier.py
+++ P4/codigos/intersection_bezier.py
@@ -3,9 +3,8 @@
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import binom
-from matplotlib.patches import Polygon
 from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
 
 class IntersectionBezier:
     def __init__(self):
@@ -26,6 +25,7 @@ class IntersectionBezier:
 
         # Calculamos interseccion
         self.intersection_points = self.intersection(P, Q)
+        print self.intersection_points
         # Returns: K puntos de interseccion calculados como un array
         # de numpy de dimensiones (K, 2)
         return self.intersection_points
@@ -33,33 +33,51 @@ class IntersectionBezier:
     
     def intersection(self, P, Q):
         if not self._boxes_overlap(P,Q):
-            return np.array([])
+            return np.array([[]])
         m = P.shape[0]-1
-        if (m*(m-1)*np.max(np.linalg.norm(P[0:m-2]+P[2:m],axis=1))) > self.epsilon:
+        b = P[0:m+1]-2*np.concatenate((P[1:m+1],[[0,0]]),axis=0)+np.concatenate((P[2:m+1],[[0,0],[0,0]]),axis=0)
+        print "-",b
+        B= np.linalg.norm(b,axis=1)# 
+        print "*",B
+        print P
+        print "----"
+        print Q
+        print "----***"
+        if np.random.rand() > 0.99:                  
+        #if ((m*(m-1)*np.amax(B)) > self.epsilon):
+            print "DENTRO"
             P1,P2 = self._subdivision(P)
+            print "P1-Q"
             intersec_points = self.intersection(P1,Q)
-            return np.concatenate(intersec_points,self.intersection(P2,Q))
+            print "P2-Q"
+            return np.concatenate((intersec_points,self.intersection(P2,Q)))
         n = Q.shape[0]-1
-        if (n*(n-1)*np.max(np.linalg.norm(Q[0:n-2]+Q[2:n],axis=1))) > self.epsilon:
+        if np.random.rand() > 0.99:
+        #if (n*(n-1)*np.amax(np.linalg.norm(Q[0:n-2]+Q[2:n],axis=1))) > self.epsilon:
             Q1,Q2 = self._subdivision(Q)
+            print "P-Q1"
             intersec_points = self.intersection(P,Q1)
-            return np.concatenate(intersec_points,self.intersection(P,Q2))
+            print "P-Q2"
+            return np.concatenate((intersec_points,self.intersection(P,Q2)))
+        print "holi"
         return self._intersect_segment(P[0],P[m],Q[0],Q[n])
 
 
     def plot(self, k=3):
         # Metodo que produce el dibujo de las curvas y las
         # intersecciones.
-        if k == 0:
-        pass
-
+        self.window = Graphicalica()
+        self._plot(self.P,k,'b')
+        self._plot(self.Q,k,'r')
+        self.window.drawPoints(self.intersection_points,'g')
+        self.window.show()
     # PRIVATE METHODS #
     #-----------------#
 
     def _boxes_overlap(self,P,Q):
-        maxs_P,mins_P = np.amax(P,axis=0),np.amin(P,axis=0)
+        maxs_P,mins_P = np.max(P,axis=0),np.min(P,axis=0)
         maxs_Q,mins_Q = np.amax(Q,axis=0),np.amin(Q,axis=0)
-        return ( (mins_P[0] < maxs_Q[0]) and (maxs_P[0] > mins_Q[0]) and (mins_P[1] < maxs_Q[1]) and (max_P[1] > mins_Q[1]) )
+        return ( (mins_P[0] < maxs_Q[0]) and (maxs_P[0] > mins_Q[0]) and (mins_P[1] < maxs_Q[1]) and (maxs_P[1] > mins_Q[1]) )
     
     def _side(self,A,B,C):
         # X - Y > 0 ==> X > Y
@@ -70,10 +88,10 @@ class IntersectionBezier:
         #comprobamos que se cortan:
         if ((self._side(A,C,D) and self._side(B,C,D)) 
             or (self._side(A,B,C) and self._side(A,B,D))):
-            return np.array([])
+            return np.array([[]])
 
         # falta calcular el punto de corte!
-        pass # devolver el punto en un numpy array
+        return np.array([[0,0]])
         
     def _subdivision(self,P):
         N = P.shape[0] - 1
@@ -90,35 +108,33 @@ class IntersectionBezier:
         div2[:,:] = compuesto[:,0,:]
         return (div1,div2)
         
-
-
-
-
-class Graphic:
+    def _plot(self,P,k,colour):
+        if k == 0:
+            self.window.drawLine(Line2D(P[:,0],P[:,1]))
+        else:
+            div1,div2 = self._subdivision(P)
+            self._plot(div1, k-1,colour)
+            self._plot(div2, k-1,colour)
+            
+            
+class Graphicalica:
     def __init__(self):
-        pass
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, aspect=1)
+        self.ax.set_xlim(-20,20)
+        self.ax.set_ylim(-20,20)
+        
+    def drawLine(self,line):
+        self.ax.add_line(line)
+        self.fig.canvas.draw()
 
+    def drawPoints(self,points,colour):
+        for p in points:
+            self.ax.add_patch(Circle((p[0],p[1]),0.3))
+        self.fig.canvas.draw()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def show(self):
+        plt.show()
 
         
 if __name__ == '__main__':
@@ -131,7 +147,10 @@ if __name__ == '__main__':
     # Puntos aleatorios para generar las curvas de Bezier
     P = np.random.uniform(-20, 20, (N + 1, 2))
     Q = np.random.uniform(-20, 20, (N + 1, 2))
-
+    points = [[0,0],[20,0],[20,20]]
+    P = np.array(points)
+    points = [[0,0],[20,0],[20,20]]
+    Q = np.array(points)
     intersect(P, Q, epsilon) # Llamada al __call__ de la clase
 
-    intersect.plot()
+    intersect.plot(0)
