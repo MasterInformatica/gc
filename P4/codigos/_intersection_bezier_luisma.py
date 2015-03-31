e90775d4f7ba628e3d6d0d22d6df82ccddf97f39
Jesús Doménech <jesusjda@gmail.com>
2015-03-31 02:55:20 +0000

    IT WORKS! No aseguro velocidad.. hay que revisar los TIEMPOS
    Para ver lo que he hecho hacer estas cosas:
    1) Poner Puntos con izquierdo y derecho del raton (con dos de cada mola)
    2) Mover Puntos
    3) Pulsar random
    4) Mover Puntos
    5) Leer codigo averiguar que hice y comentar... (no se si lo voy a saber yo)
    
    El codigo ha quedado algo enrevesado le dare otra vuelta para ver si se puede simplificar...
    
    He tenido que add una condicion donde la delta cuadrado de b... ya que si solo habia dos puntos.. no tenia sentido..
    
    No se si os mola...
    
    TO DO:
    a) Comentario en plot diciendo lo del boton derecho e izquierdo
    b) Revisar los tiempos de lo obligatorio
    c) Main con un Booleano para interactivo o standar con ejemplo
    d) Matar a vuestro compy jesus que lo complica todo
    e) Comentar el codigo
    f) Usar el tipo de comentario de las dobles comillas si conseguimos que funcione aunque emacs lo tabule mal
    
    Eso es todo buenas noches
    Prohibido mirar la hora del commit 
    
    Chauu

--- P4/codigos/_intersection_bezier_luisma.py
+++ P4/codigos/_intersection_bezier_luisma.py
@@ -37,12 +37,11 @@ class IntersectionBezier:
         if not self._boxes_overlap(P,Q):
             return np.array([])
 
-
         m = P.shape[0]-1 #apunta al ultimo indice accesible
-        #Delta2(b_i) = b_(i+2) - 2 b_(i+1) + b_i     i=0...m-2
+        # Delta2(b_i) = b_(i+2) - 2 b_(i+1) + b_i     i=0...m-2
         b =           P[2:m+1] - 2*P[1:m]  + P[0:m-1]
-        B = np.linalg.norm(b, axis=1)
-        if ((m*(m-1)*np.amax(B)) > self.epsilon):
+        norm_b = np.linalg.norm(b, axis=1)
+        if norm_b.shape[0] != 0 and ((m*(m-1)*np.amax(norm_b)) > self.epsilon):
             P1, P2 = self._subdivision(P)
             ####
             # ARREGLAR: Aquí habría que coger solo los que no son duplicados
@@ -62,9 +61,8 @@ class IntersectionBezier:
 
         n = Q.shape[0]-1
         b = Q[2:n+1] - 2*Q[1:n] + Q[0:n-1]
-        B = np.linalg.norm(b, axis=1)
-        #if np.random.rand() > 0.99:
-        if (n*(n-1)*np.amax(B)) > self.epsilon:
+        norm_b = np.linalg.norm(b, axis=1)
+        if norm_b.shape[0] != 0 and (n*(n-1)*np.amax(norm_b)) > self.epsilon:
             Q1, Q2 = self._subdivision(Q)
             ####
             # ARREGLAR: Aquí habría que coger solo los que no son duplicados
@@ -88,9 +86,6 @@ class IntersectionBezier:
         # intersecciones.
         if(self.window == None):
             self.window = Graphicalica()
-        else:
-            self.window.clean()
-
         
         self._plot(self.P,k,'b')
         self._plot(self.Q,k,'r')
@@ -119,19 +114,17 @@ class IntersectionBezier:
 
     def _intersect_segment(self,A,B,C,D):
         # segmento A-B y C-D
-        #comprobamos que se cortan:
+        # comprobamos que se cortan:
         if ((self._side(A,C,D) and self._side(B,C,D)) 
             or (self._side(A,B,C) and self._side(A,B,D))):
             return np.array([])
 
-        # falta calcular el punto de corte!
+        # calculamos el punto de corte!
         line1 = np.cross([A[0],A[1],1], [B[0],B[1],1])
         line2 = np.cross([C[0],C[1],1], [D[0],D[1],1])
-       #  print 'l1', line1
-        # print 'l2', line2
 
         intersect_point = np.cross(line1, line2)
-        #print 'inters', intersect_point
+
         return np.array([[intersect_point[0]/intersect_point[2], intersect_point[1]/intersect_point[2]]])
         
         
@@ -152,12 +145,14 @@ class IntersectionBezier:
         
     def _plot(self,P,k,colour):
         if k == 0:
+
             self.window.drawLine(Line2D(P[:,0],P[:,1], color=colour))
         else:
             div1, div2 = self._subdivision(P)
             self._plot(div1, k-1, colour)
             self._plot(div2, k-1, colour)
-            
+    def eraseLines(self):
+        self.ax.lines= []
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
@@ -168,7 +163,18 @@ class Graphicalica:
         self.ax = self.fig.add_subplot(111, aspect=1)
         self.ax.set_xlim(-20,20)
         self.ax.set_ylim(-20,20)
-        
+        self.points_P = []
+        self.points_Q = []
+        self.inter_circle = []
+        # Variables para Mover puntos
+        self.exists_touched_circle = False
+        self.touched_P = True
+        self.touched_index = None
+        self.touched_x0, self.touched_y0 = None,None
+        # Controlador de Eventos
+        self.cid_press = self.fig.canvas.mpl_connect('button_press_event', self.on_press)        
+        self.cid_move = self.fig.canvas.mpl_connect('motion_notify_event', self.on_move)
+        self.cid_release_button = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
 
     def init_interaction(self, bezier):
         self.bezier = bezier
@@ -188,14 +194,17 @@ class Graphicalica:
         self.kSlider.on_changed(self._updateK)
 
         #Buttons
-        calculateAxes = plt.axes([0.7, 0.15, 0.15, 0.03])
-        resetAxes = plt.axes([0.7, 0.1, 0.15, 0.03])
-
+        calculateAxes = plt.axes([0.7, 0.17, 0.15, 0.03])
+        randomAxes = plt.axes([0.7, 0.11, 0.15, 0.03])
+        resetAxes = plt.axes([0.7, 0.05, 0.15, 0.03])
+        
         self.buttonCalculate = Button(calculateAxes, 'Calculate!')
         self.buttonReset = Button(resetAxes, 'Reset!')
+        self.buttonRandom = Button(randomAxes, 'Random!')
 
         self.buttonCalculate.on_clicked(self._updatePlot)
         self.buttonReset.on_clicked(self._clean)
+        self.buttonRandom.on_clicked(self._randomPlot)
 
 
     def _updateEps(self, val):
@@ -205,34 +214,120 @@ class Graphicalica:
         self.k = int(val)
 
     def _updatePlot(self, event):
+        P = np.array(self.points_P)
+        Q = np.array(self.points_Q)
+        if P.shape[0] < 1 or Q.shape[0] < 1: # no hay puntos suficientes
+            return
+        self.ax.lines = []
+ 
+        cuts = self.bezier(P, Q, self.eps) #Llamada al __call__ de la clase
+        for c in self.inter_circle: #reset intersection Points
+            c.remove()
+        self.inter_circle = []
+        self.bezier.plot(k=self.k)
+
+    def _randomPlot(self, event):
         cuts = np.array([])
         N = 5
+
         while (cuts.shape[0] < 3):
+            self.clean()
             P = np.random.uniform(-20, 20, (N + 1, 2))
             Q = np.random.uniform(-20, 20, (N + 1, 2))
+            # Para poder mover los puntos
+            for p in P:
+                self.points_P.append([p[0],p[1]])       
+                c = Circle((p[0],p[1]), 0.5,color='b')
+                self.ax.add_patch(c)
+            for q in Q:
+                self.points_Q.append([q[0],q[1]])       
+                c = Circle((q[0],q[1]), 0.5,color='r')
+                self.ax.add_patch(c)
             cuts = self.bezier(P, Q, self.eps) #Llamada al __call__ de la clase
 
         self.bezier.plot(k=self.k)
-
 
     def drawLine(self,line):
         self.ax.add_line(line)
         self.fig.canvas.draw()
 
     def drawPoints(self,points,colour):
-        if points.shape[1] != 0:
+        if points.shape[0] != 0:
             for p in points:
-                self.ax.add_patch(Circle((p[0],p[1]),0.3))
+                c = Circle((p[0],p[1]),0.3,color='g')
+                self.inter_circle.append(c)
+                self.ax.add_patch(c)
         self.fig.canvas.draw()
 
     def show(self):
         plt.show()
 
     def clean(self):
+        self.points_P = []
+        self.points_Q = []
+        for c in self.inter_circle: #reset intersection Points
+            c.remove()
+        self.inter_circle = []
         self.ax.cla()
 
     def _clean(self, event):
         self.clean()
+
+    def on_press(self, event):
+        if not self.ax.contains(event)[0]: #press out of plot
+            return
+        for circle in self.ax.patches:
+            contains, attr = circle.contains(event)
+            if contains:
+                self.touched_circle = circle
+                self.exists_touched_circle = True
+                self.pressed_event = event
+                self.touched_x0, self.touched_y0 = circle.center
+                punto = [self.touched_x0, self.touched_y0]
+                if punto in self.points_P:
+                    self.touched_index = self.points_P.index(punto)
+                    self.touched_P = True
+                else:
+                    self.touched_index = self.points_Q.index(punto)
+                    self.touched_P = False
+                return
+            
+        if event.button == 3: #boton derecho
+            self.points_Q.append([event.xdata, event.ydata])       
+            c = Circle((event.xdata, event.ydata), 0.5,color='r')
+        elif event.button == 1:
+            self.points_P.append([event.xdata, event.ydata])
+            c = Circle((event.xdata, event.ydata), 0.5,color='b')
+        else:
+            return
+        self.ax.add_patch(c)
+        self.fig.canvas.draw()
+        self._updatePlot(event)
+
+
+    def on_release(self, event):
+        # Reseteamos la variable al soltar el boton del raton
+        self.exists_touched_circle = False
+        return
+
+    def on_move(self, event):
+        if not self.ax.contains(event)[0]: #press out of plot
+            return
+        # Si habiamos pinchado encima de un circulo existente y lo intentamos
+        # mover, se actualiza el poligono en consecuencia, se modifican los
+        # valores
+        if self.exists_touched_circle:
+            dx = event.xdata - self.pressed_event.xdata
+            dy = event.ydata - self.pressed_event.ydata
+            x0, y0 = self.touched_circle.center
+            self.touched_circle.center = self.touched_x0+dx, self.touched_y0+dy
+            #Actualizar arrays
+            if self.touched_P:
+                self.points_P[self.touched_index] = [self.touched_x0+dx, self.touched_y0+dy]
+            else:
+                self.points_Q[self.touched_index] = [self.touched_x0+dx, self.touched_y0+dy]
+            self._updatePlot(event)
+            #self.fig.canvas.draw()
         
 if __name__ == '__main__':
     N = 5  
