4f181e13e6290c1666645d0e59469924e69693c7
luismacostero <lcostero@ucm.es>
2015-04-06 14:12:19 +0000

    añaidos comentarios de las clases

--- P4/codigos/_intersection_bezier_luisma.py
+++ P4/codigos/_intersection_bezier_luisma.py
@@ -7,9 +7,14 @@ from matplotlib.lines import Line2D
 from matplotlib.patches import Circle
 from matplotlib.widgets import Slider, Button
 
+
 class IntersectionBezier:
+    """
+    Clase que se encarga de dadas dos curvas, implementa los métodos para
+    buscar la intersección de estas y pintarlas en la ventana gráfica
+    """
+
     def __init__(self):
-        # Aqui habra cosas bonitas para la creacion del Intersection
         self.intersection_points = None
         self.epsilon = None
         self.P = None
@@ -17,30 +22,43 @@ class IntersectionBezier:
         self.window = None
 
     def __call__(self, P, Q, epsilon):
-        # P.shape = (n, 2)
-        # Q.shape = (m,2)
-        # Epsilon controlara si se aproxima el poligono 
-        #   de control mediante un segmento
+        """
+        Calcula la intersección de dos curvas de Bezier. Llama internamente 
+        al método que lo calcula
+        
+        P: Polígono de control de la primera curva. P.shape = (n, 2)
+        Q: polígono de control de la segunda curva. Q.shape = (m,2)
+        epsilon: Inidica la aproximación del método para subdividir
+        """
+
         self.epsilon = epsilon
         self.P = P
         self.Q = Q
 
-        # Calculamos interseccion
+        # Calculamos interseccion, almacenamos y devolvemos
         self.intersection_points = self.intersection(P, Q)
-        # Returns: K puntos de interseccion calculados como un array
-        # de numpy de dimensiones (K, 2)
         return self.intersection_points
 
     
     def intersection(self, P, Q):
+        """
+        Calcula la intersección de dos curvas de Bezier. Llama internamente 
+        al método que lo calcula
+        
+        P: Polígono de control de la primera curva. P.shape = (n, 2)
+        Q: polígono de control de la segunda curva. Q.shape = (m,2)
+        """
+
+        # Test si las cajas se cortan
         if not self._boxes_overlap(P,Q):
             return np.array([])
+
 
         m = P.shape[0]-1 #apunta al ultimo indice accesible
         # || Delta2(b_i) ||= || b_(i+2)  - 2*b_(i+1) + b_i ||          i=0...m-2
         norm_b = np.linalg.norm(P[2:m+1] - 2*P[1:m]  + P[0:m-1] , axis=1)
-        if norm_b.shape[0] != 0 and ((m*(m-1)*np.amax(norm_b)) > self.epsilon):
 
+        if norm_b.shape[0] != 0 and ((m*(m-1)*np.amax(norm_b)) > self.epsilon):
             P1, P2 = self._subdivision(P)
             intersec_points1 = self.intersection(P1,Q)
             intersec_points2 = self.intersection(P2,Q)
@@ -54,7 +72,6 @@ class IntersectionBezier:
                     if not (np.amin(np.linalg.norm(intersec_points1 - p)) < self.epsilon):
                         intersec_points1 = np.append(  intersec_points1 ,  [p], axis=0)
                 return intersec_points1
-                #return np.concatenate((intersec_points1, intersec_points2), axis=0)
 
 
         n = Q.shape[0]-1 
@@ -76,11 +93,20 @@ class IntersectionBezier:
                         intersec_points1 = np.append(  intersec_points1 ,  [p], axis=0)
                 return intersec_points1
 
+
         return self._intersect_segment(P[0],P[m],Q[0],Q[n])
 
+
+
     def plot(self, k=3):
-        # Metodo que produce el dibujo de las curvas y las
-        # intersecciones.
+        """
+        Inicializa la ventana gráfica. Y pinta las curvas generadas por subdivision y los
+        puntos de intersección de ambas. Llama internamente a otro método encargado de pintar.
+        OJO: es necesario llamar primero a __call__ para añadir los polígonos a la clase.
+
+        k: número de subdivisiones antes de dibujar las curvas
+        """
+
         if(self.window == None):
             self.window = Graphicalica()
         
@@ -91,31 +117,46 @@ class IntersectionBezier:
 
 
     def interactive(self):
+        """
+        Inicializa la clase para ser interactiva. Crea la ventana gráfica, la inicializa
+        y le pasa self, instancia encargada de intersecar curvas y pintarlas mediante
+        subdivisiones
+        """
         self.window = Graphicalica()
         self.window.init_interaction(self)
         self.window.show()
 
 
-
+        
+    #-----------------#
     # PRIVATE METHODS #
     #-----------------#
-
     def _boxes_overlap(self,P,Q):
-        maxs_P,mins_P = np.max(P,axis=0),np.min(P,axis=0)
-        maxs_Q,mins_Q = np.amax(Q,axis=0),np.amin(Q,axis=0)
+        """
+        Comprueba si las cajas de los poígonos P, Q se intersecan
+        """
+        maxs_P,mins_P = np.max(P,axis=0), np.min(P,axis=0)
+        maxs_Q,mins_Q = np.amax(Q,axis=0), np.amin(Q,axis=0)
         return ( (mins_P[0] < maxs_Q[0]) and (maxs_P[0] > mins_Q[0]) and (mins_P[1] < maxs_Q[1]) and (maxs_P[1] > mins_Q[1]) )
     
+        
     def _side(self,A,B,C):
+        """
+        Devuleve true/false según a que lado esté el punto respecto al segmento
+        """
         # X - Y > 0 ==> X > Y
         return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
 
     def _intersect_segment(self,A,B,C,D):
-        # segmento A-B y C-D
+        """
+        Devuelve el punto de instercción entre el segmento AB y CD. 
+        """
+        
         # comprobamos que se cortan:
         if ((self._side(A,C,D) == self._side(B,C,D)) 
             or (self._side(A,B,C) == self._side(A,B,D))):
             return np.array([])
-
+            
         # calculamos el punto de corte!
         line1 = np.cross([A[0],A[1],1], [B[0],B[1],1])
         line2 = np.cross([C[0],C[1],1], [D[0],D[1],1])
@@ -126,6 +167,13 @@ class IntersectionBezier:
         
         
     def _subdivision(self,P):
+        """
+        Dado el polígono P, devuelve los dos polígonos de control resultantes de
+        subdividir el polígono P.
+
+        P: Polígono a subdividir
+        """
+
         N = P.shape[0] - 1
         #                       k    N  (x,y)
         compuesto =  np.zeros((N+1, N+1,  2)) 
@@ -140,9 +188,18 @@ class IntersectionBezier:
         div2[:,:] = compuesto[:,0,:]
         return (div1,div2)
         
-    def _plot(self,P,k,colour):
-        if k == 0:
 
+    def _plot(self,P,k,colour):
+        """
+        Pinta la curva asociada al polígono P. Realiza k subdivisiones antes de pintar la
+        curva en la venta gráfica con el color colour
+
+        P: Polígono de control de la curva a dibujar
+        k: Número de subdivisiones antes de pintar la curva
+        colour: color con el que se va a pintar la curva
+        """
+
+        if k == 0:
             self.window.drawLine(Line2D(P[:,0],P[:,1], color=colour))
         else:
             div1, div2 = self._subdivision(P)
@@ -329,20 +386,10 @@ class Graphicalica:
             self.fig.canvas.draw()
         
 if __name__ == '__main__':
-    N = 5  
-    epsilon = 0.001
+    """
+    Si iniciamos la aplicación de manera directa, inicilizamos la ventana
+    gráfica para ser interactiva
+    """
 
-    # Instancia de la clase
     intersect = IntersectionBezier()
-    
     intersect.interactive();
-
-    # # Puntos aleatorios para generar las curvas de Bezier
-    # cuts = np.array([])
-    # while (cuts.shape[0] < 3):
-    #     P = np.random.uniform(-20, 20, (N + 1, 2))
-    #     Q = np.random.uniform(-20, 20, (N + 1, 2))
-    #     cuts = intersect(P, Q, epsilon) # Llamada al __call__ de la clase
-
-    # intersect.plot()
-
