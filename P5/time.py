d72860bf52a2c29eb38bc2b32e44d990d1f9674d
Jenny Hernández <jhdez.becares@gmail.com>
2015-04-08 09:26:24 +0000

    Timer

---
+++ P5/time.py
@@ -0,0 +1,11 @@
+import time
+
+t = time.time()
+for i in range(10000000):
+	pass
+
+print time.time() - t
+
+# En nuestra maquina del lab: 0.498979091644
+# En su maquina del lab: 0.447
+# En el pc de Luisma: 0.862239
