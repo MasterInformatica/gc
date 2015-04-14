fcf60d5f658afbf7e412c0c091447e5a23d323b2
Jenny Hernández <jhdez.becares@gmail.com>
2015-04-14 14:32:54 +0000

    Chapuza, cuidado

---
+++ P5/codigos/curve_fitting.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+def polynomial_curve_fitting(points, knots, method, L=0, libraries=False, num_points=100):
+    '''
+       Fits planar curve to points at given knots. 
+
+       Arguments:
+           points -- coordinates of points to adjust (x_i, y_i) given by a numpy array of shape (N, 2)
+           knots -- strictly increasing sequence at which the curve will fit the points, tau_i
+               It is given by a np.array of shape M, unless knots='chebyshev', in this case
+                   N Chebyshev's nodes between 0 and 1 will be used instead of tau.
+           method -- one of the following: 
+               'newton' computes the interpolating polynomial curve using Newton's method.
+                   returns error if N!=M. 
+               'least_squares' computes the best adjusting curve in the least square sense,
+                   i.e., min_a ||Ca - b||**2 + L/2 ||a||**2
+           L -- regularization parameter
+           libraries -- If False, only numpy linear algebra operations are allowed. 
+               If True, any module can be used. In this case, a very short and fast code is expected
+           num_points -- number of points to plot between tau[0] and tau[-1]
+
+       Returns:
+           numpy array of shape (num_points, 2) given by the evaluation of the polynomial
+           at the evenly spaced num_points between tau[0] and tau[-1]
+       '''
+
+       pass
+
+
+
+
+
+
+                                 
+                
