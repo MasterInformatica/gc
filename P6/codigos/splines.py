c909d290bf65a9c77359f6fab4119fc7bf836fd7
Jesús Doménech <jesusjda@gmail.com>
2015-05-04 11:21:51 +0000

    Ahora funciona... sabes...

--- P6/codigos/splines.py
+++ P6/codigos/splines.py
@@ -32,39 +32,25 @@ def spline2d(a, b, xi, k, nu, A, num_dots):
        Returns:
        the spline curve as a numpy array of size (2, num_dots) <'''
     A = np.array(A)
-    sol = np.zeros((2, num_dots))
-    sol[0] = spline1d(a, b, np.array(xi), k, np.array(nu), A[:,0], num_dots)
-    sol[1] = spline1d(a, b, np.array(xi), k, np.array(nu), A[:,1], num_dots)
-    return sol
-
-
-
-
-def spline1d(a, b, xi, k, nu, A, num_dots):
+    xi = np.array(xi)
+    nu = np.array(nu)
 
     var = Vars_spline(a, b, xi, k, nu, A, num_dots)
     n_ts = k*(1+nu.shape[0]+1)-np.sum(nu)
-    s = np.zeros(num_dots)
+    s = np.zeros((2,num_dots))
     tau = var.get_tau()
     t = var.get_t()
     index = 0
-    suma = 0
+
     for i_tau in range(num_dots):
-#        suma = 0
-#        for i in range(t.shape[0]-2):
-#            if( t[i] <= tau[i_tau] and tau[i_tau] <= t[i+1]):
-#                suma +=var.get_a(k-1,i)[i_tau]
-#        if( t[-2] <= tau[i_tau] and tau[i_tau] <= t[-1]):
-#            suma += var.get_a(k-1,t.shape[0]-1)[i_tau]
-#        s[i_tau] = suma
-#    print s
         # Paso 1: Avanzar el indice hasta que se pueda operar
-        while (index < n_ts-3 and t[index+1] <= tau[i_tau]):
+        while (index < n_ts-2 and t[index+1] <= tau[i_tau]):
             index += 1
         
         # Paso 2: Sumamos a(k-1,index,tau)
         if (t[index] <= tau[i_tau] and tau[i_tau] <= t[index+1]):
-            s[i_tau] = (var.get_a(k-1,index))[i_tau]
+            s[:,i_tau] =  (var._calc_a(k-1,index))[:,i_tau]
+
     return s
     
 
@@ -72,17 +58,15 @@ class Vars_spline:
     def __init__(self, a, b, xi, k, nu, A, num_dots):
         self.tau = np.linspace(a, b, num_dots)
         self.w = {}
-        self.t_i = np.zeros( (k)*(1+nu.shape[0]+1)-sum(nu))
+        self.t_i = np.zeros( k*(1+nu.shape[0]+1)-sum(nu)+1)
         self.a = {}  #np.empty((A.shape[0],A.shape[0]))
         self.k = k
         #calc variables
         self._calc_t(a,b,xi,k,nu)
-        
-        for i in range(A.shape[0]):
-            self.a[(0,i)] = np.full(num_dots,A[i])
-
-    def get_w(self,i,k):
-        return self._calc_w(i,k)
+        self.A = A
+        self.num_dots = num_dots
+#        for i in range(A.shape[0]):
+#            self.a[(0,i)] = np.full(num_dots,A[i])
 
     def get_tau(self):
         return self.tau
@@ -90,27 +74,29 @@ class Vars_spline:
     def get_t(self):
         return self.t_i
 
-    def get_a(self,r,i):
-        return self._calc_a(r,i)
-
     def _calc_w(self,i,k):
-#        print "El la llamada a w, las variables valen: i:", i," k:",k
         if( (i,k) in self.w):
             return self.w[(i,k)]
+        if (self.t_i[i] == self.t_i[i+k-1]):
+            self.w[(i,k)] = np.zeros(self.tau.shape[0])
         else:
-            if (self.t_i[i] == self.t_i[i+k-1]):
-                self.w[(i,k)] = np.zeros(self.tau.shape[0])
-            else:
-                self.w[(i,k)] = ((self.tau - self.t_i[i])*1.0/(self.t_i[i+k-1]-self.t_i[i])*1.0)
-            return self.w[(i,k)]
+            self.w[(i,k)] = ((self.tau - self.t_i[i])*1.0/(self.t_i[i+k-1]-self.t_i[i])*1.0)
+        return self.w[(i,k)]
 
     def _calc_a(self,r,i):
         if ((r,i) in self.a):
             return self.a[(r,i)]
+        if (r==0):
+            self.a[(r,i)]=self.A[i]
+            return self.a[(r,i)]
         wi = self._calc_w(i,self.k-r+1)
         ai_1 = self._calc_a(r-1,i-1)
         ai = self._calc_a(r-1,i)
-        self.a[(r,i)] = (1-wi)*ai_1 + wi*ai
+        aux = np.zeros((2,self.num_dots))
+        aux[0] = (1-wi)*ai_1[0] + wi*ai[0]
+        aux[1] = (1-wi)*ai_1[1] + wi*ai[1]
+
+        self.a[(r,i)] = aux
         return self.a[(r,i)]
 
    
@@ -120,33 +106,42 @@ class Vars_spline:
 
     def _calc_t(self,a,b,xi,k,nu):
         l = nu.shape[0]
-
         index=0
         self.t_i[0:k] = a
-        index = k+1;
+        index = k+1
         for i in range(0, l):
             self.t_i[index : index+(k-nu[i])] = xi[i] 
             index += (k-nu[i])
-
         self.t_i[index: index+k] = b
 
 
 
-def menu():
+    
+
+def menu1():
     A = [[-3, 3], [-3, 3], [-3, 3], [8, 2], [-4, 8], [0, 8], [-1, 0], [-1, 0], [-1, 0]]
     s = spline2d(0, 4, [1,2,3], 3, [2,2,2], A, 100)
-    print s
+    print s[0]
+    print s[1]
     nA = np.array(A)
-    #for i in range(nA.shape[0]):
-    # plt.plot(nA[i][0], nA[i][1], 'ro')
-
+    plt.plot(nA[:,0],nA[:,1],'-o')
     plt.plot(s[0],s[1])
-    plt.plot(nA[:,0],nA[:,1], 'r-o')
-    plt.axis([-10, 10, -10, 10])
     plt.show()
+
+def menu2():
+    A = [[5, -4], [5, -4], [8, -1], [-9, -8], [7, 7], [5, 7], [5, 7]]
+    s = spline2d(0, 4, [1,2,3], 2, [1,1,1], A, 10000)
+    print s[0]
+    print s[1]
+    nA = np.array(A)
+    plt.plot(nA[:,0],nA[:,1],'-o')
+    plt.plot(s[0],s[1])
+    plt.show()
+
     
-if __name__=="__main__":
-    menu()
+#if __name__=="__main__":
+#    menu1()
+#    menu2()
     # N = 1000
     # start = time.time()
     # for i in range(N):
