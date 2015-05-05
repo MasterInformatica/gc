3e2d72f8b9acaefc960fccc2670e3c989ac27fad
Jenny Hernández <jhdez.becares@gmail.com>
2015-05-05 08:51:31 +0000

    Añadido un esquemilla del algoritmo malo

---
+++ P7_ConvexHull/basura_jenny/py.py
@@ -0,0 +1,31 @@
+from __future__ import division
+import numpy as np
+
+def right_turn(three_points):
+    pass
+    
+
+def menu(points):
+    points = np.array(points)
+
+    # Sort the points by x-coordinate, resulting in a sequence p1,...,pn
+    ordered_points = pass
+
+    # Put the points p1 and p2 in a list Lupper, with p1 as the first point
+    Lupper = [ordered_points[0], ordered_points[1]]
+    
+    for i in range(3,n): # n incluida????
+        Lupper.append(ordered_points[i])
+        # while Lupper contains more than two points and the last three points in Lupper do not make a right turn: delete the middle of the last three points from Lupper
+        while len(Lupper) > 2 and right_turn(pass)==false:
+
+    # put the points pn and pn-1 in a list Llower, with pn as the first point
+    Llower = pass
+    # To be continued... 
+
+    # Faltan cosas desde la linea 8
+
+
+if __name__ == '__main__':
+    points = [[1,2],[1.5,2.5],[3,1],[1.2,3.1]]
+    menu(points)
