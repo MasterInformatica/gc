6f1bee01ca592806fcb09e26b71255400c7859d2
Jesús Doménech <jesusjda@gmail.com>
2015-05-12 08:08:29 +0000

    avances en quickhull

--- P7_ConvexHull/codigos/quickhull.py
+++ P7_ConvexHull/codigos/quickhull.py
@@ -11,24 +11,34 @@ Autores:
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
 
 def maxDistancePoint(a,b,points):
-    P = np.array(points)
-    D = np.linalg.det(np.array([[b-a],[P-a ]]))
-    print D
-    return np.max(D/np.norm(a,b))
+    return points[0]
 
-def rigth_Points(a,b,points):
-    pass
+def right_turn(a, b, c):
+    return (b[0]-a[0])*(c[1]-a[1])-(b[1]-a[1])*(c[0]-a[0])
+    
+def right_Points(a,b,points):
+    R = []
+    L = []
+    for p in points:
+        # v = right_turn(a,b,p)
+        v = (b[0]-a[0])*(p[1]-a[1])-(b[1]-a[1])*(p[0]-a[0])
+        if(v<0):
+            R.append(p)
+        elif(v>0):
+            L.append(p)
+    return R,L
 
 def _quickHull(a,b,points):
     if(len(points)<=1):
         return points
     else:
         c = maxDistancePoint(a,b,points)
-        print c 
-        A = right_Points(a,c,points)
-        B = right_Points(c,b,points)
+        print c
+        A,_ = right_Points(a,c,points)
+        B,_ = right_Points(c,b,points)
         return _quickHull(a,c,A)+c+_quickHull(c,b,B)
 
 def extremos(points):
@@ -46,10 +56,10 @@ def extremos(points):
     
 def quickHull(points):
     m,M =extremos(points)
-    A = right_Points(m,M,points)
-    B = right_Points(M,m,points)
+    A,B = right_Points(m,M,points)
+    print m, M
     return [m]+_quickHull(m,M,A)+[M]+_quickHull(M,m,B)
 
 if __name__ == '__main__':
-    
-    quickHull( [[-1, 0], [1, 0], [1, 1], [-1, 0], [1, 0], [1, 1]])
+    # print right_Points([0,0],[4,0],[[1,1],[2,1],[1,-1],[2,-1],[2,0]])
+    print quickHull( [[-1, 0], [1, 0], [1, 1], [-1, 0], [1, 0], [1, 1]])
