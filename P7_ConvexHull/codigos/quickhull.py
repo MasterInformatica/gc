087bd33a35fd46007eec80ccfd2283f95fd5b3d1
Jesús Doménech <jesusjda@gmail.com>
2015-05-13 08:20:30 +0000

    iniciando numpy

--- P7_ConvexHull/codigos/quickhull.py
+++ P7_ConvexHull/codigos/quickhull.py
@@ -14,16 +14,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 
 def maxDistancePoint(a,b,points):
-    return points[np.argmax(distance(a,b,points))]
-
-def distance(A, B, P):
-    """ segment line AB, point P, where each one is an array([x, y]) """
-
-    if np.arccos(np.dot((P - A) / np.norm(P - A), (B - A) / np.norm(B - A))) > np.pi / 2:
-        return np.norm(P - A)
-    if np.arccos(np.dot((P - B) / np.norm(P - B), (A - B) / np.norm(A - B))) > np.pi / 2:
-        return np.norm(P - B)
-    return np.abs(np.dot(A - B, P[::-1]) + np.det([A, B])) / np.norm(A - B) 
+    return points[0]
 
 def right_turn(a, b, c):
     return (b[0]-a[0])*(c[1]-a[1])-(b[1]-a[1])*(c[0]-a[0])
@@ -46,29 +37,52 @@ def _quickHull(a,b,points):
     else:
         c = maxDistancePoint(a,b,points)
         print c
-        A,X = right_Points(a,c,points)
-        B,X = right_Points(c,b,points)
-        return _quickHull(a,c,A)+[c]+_quickHull(c,b,B)
+        A,_ = right_Points(a,c,points)
+        B,_ = right_Points(c,b,points)
+        return _quickHull(a,c,A)+c+_quickHull(c,b,B)
 
 def extremos(points):
     maxp = points[0]
     minp = points[0]
     for p in points:
         # calculo maximo
-        if p[0] > maxp[0] or (p[0]==maxp[0] and p[1] < maxp[1]):
+        if p[0] > maxp[0] or (p[0]==maxp[0] and p[1] > maxp[1]):
             maxp = p
             continue
         # calculo minimo
-        if p[0] < minp[0] or (p[0]==minp[0] and p[1] > minp[1]):
+        if p[0] < minp[0] or (p[0]==minp[0] and p[1] < minp[1]):
             minp = p
     return (minp,maxp)
     
-def convex_hull(points):
+def quickHull(points):
     m,M =extremos(points)
     A,B = right_Points(m,M,points)
-    return [m]+_quickHull(m,M,A)+[M]+_quickHull(M,m,B)+[m]
+    print m, M
+    return [m]+_quickHull(m,M,A)+[M]+_quickHull(M,m,B)
 
 
-if __name__ == "__main__":
-    points  = [[1,0],[-1,0],[1,1],[1,0],[-1,0],[1,1]]
-    print convex_hull( points)
+def separate(a,b,points):
+    R = np.array([[]])
+    L = np.array([[]])
+    for p in points:
+        print p
+        v = (b[0]-a[0])*(p[1]-a[1])-(b[1]-a[1])*(p[0]-a[0])
+        if(v<0):
+            R = np.append(R,[[p]])
+        elif(v>0):
+            L = np.append(L,[[p]])
+    return L,R
+
+def extremosNP(points):
+    return points[0],points[-1]
+
+def convex_hull(points):
+    points = np.array(sorted(points))
+    m,M = extremosNP(points)
+    A,B = separate(m,M,points)
+    print A,B
+    return 0
+
+if __name__ == '__main__':
+    # print right_Points([0,0],[4,0],[[1,1],[2,1],[1,-1],[2,-1],[2,0]])
+    print convex_hull( [[-1, 0], [1, 0], [1, 1], [-1, 0], [1, 0], [1, 1],[-10,-5],[10,5],[-10,0],[-10,-10],[10,0],[10,10]])
